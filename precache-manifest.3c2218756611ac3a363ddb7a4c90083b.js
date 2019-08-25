c11b493cd4fe5e73e49e6303cbf1611eb361363a
Thiago Bocchile <tykoth@gmail.com>
2019-08-25 17:50:00 +0000

    Updates

---
+++ precache-manifest.3c2218756611ac3a363ddb7a4c90083b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6875f11e8b170ad8da280cb0a76e33e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "012fa9dd869714c44ac0",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3efbdba2ab37407cb579",
+    "url": "/static/js/2.1be5bbf0.chunk.js"
+  },
+  {
+    "revision": "03fb090d2da8f424d774",
+    "url": "/static/js/3.de1c1389.chunk.js"
+  },
+  {
+    "revision": "e3ea3e26bdfd84d295db",
+    "url": "/static/js/4.edfabcf3.chunk.js"
+  },
+  {
+    "revision": "012fa9dd869714c44ac0",
+    "url": "/static/js/main.9a772ebe.chunk.js"
+  },
+  {
+    "revision": "5d42981d3d6541571695",
+    "url": "/static/js/runtime~main.7b242b34.js"
+  },
+  {
+    "revision": "5fb97c4c8ec6021fdcd7b8bc03cb0063",
+    "url": "/static/media/ezgif.com-gif-to-webm.5fb97c4c.webm"
+  },
+  {
+    "revision": "f2cf99194abce728f4ce00be0dda7ec6",
+    "url": "/static/media/saturn.f2cf9919.jpg"
+  }
+]);
