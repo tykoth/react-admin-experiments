119f2a32e50b0f91f46a7a9c329fad294386d16a
Thiago Bocchile <tykoth@gmail.com>
2021-10-25 00:28:58 -0300

    OK!

--- tools/generate.js
+++ tools/generate.js
@@ -19,20 +19,20 @@ const config = require('../package.json');
 //         },
 //     },
 // ]);
-// generateTemplateFiles([
-//     {
-//         option: 'Create React-Admin Resource',
-//         defaultCase: '(pascalCase)',
-//         entry: {
-//             folderPath: './tools/templates/react-admin/resources/',
-//         },
-//         stringReplacers: ['__resource__', '__model__'],
-//         output: {
-//             path: './src/resources/__resource__(lowerCase)',
-//             pathAndFileNameDefaultCase: '(pascalCase)',
-//         },
-//         onComplete: (results) => {
-//             console.log(`results`, results);
-//         },
-//     },
-// ]);
+ generateTemplateFiles([
+     {
+         option: 'Create React-Admin Resource',
+         defaultCase: '(pascalCase)',
+         entry: {
+             folderPath: './tools/templates/react-admin/resources/',
+         },
+         stringReplacers: ['__resource__', '__model__'],
+         output: {
+             path: './src/resources/__resource__(lowerCase)',
+             pathAndFileNameDefaultCase: '(pascalCase)',
+         },
+       onComplete: (results) => {
+             console.log(`results`, results);
+         },
+     },
+]);
